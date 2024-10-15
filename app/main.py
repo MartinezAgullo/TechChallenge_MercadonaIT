72aed70518930dac84c82435faa1b9b079acd505
MartinezAgullo <pablo.martinez.agullo@gmail.com>
2024-10-15 18:40:07 +0200

    Uplad exercice

---
+++ app/main.py
@@ -0,0 +1,16 @@
+"""
+##################################################################
+#                                                                #
+#                      MAIN REST API FILE                        #
+#                                                                #
+##################################################################
+
+This file will be the main file of your REST API.
+Use it to add an endpoint /predict that will be used to ask your model
+for predictions.
+
+You can use any technology you find suitable to develop your REST API, but make sure
+to explain how to make it work.
+"""
+
+print("🌐 Your awesome REST API")
